feccf2d06f913738d6a1b80e5fb32ff840154878
Aurel Lazar <lazar.aurel@gmail.com>
2017-09-01 07:46:02 -0700

    Initial template for raw MVC project.

---
+++ Elections/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elections.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
