88d0b0a79520f5271b3d140ac8a51450a8143537
Aurel Lazar <lazar.aurel@gmail.com>
2018-08-10 00:00:00 -0700

    Add beginning of nomination code.

---
+++ Elections/Controllers/NominateController.cs
@@ -0,0 +1,26 @@
+﻿using Elections.Models;
+using Elections.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elections.Controllers
+{
+	public class NominateController : Controller
+	{
+		private readonly ElectionContext _context;
+		private readonly Mailer _mailer;
+
+		public NominateController(ElectionContext context, Mailer mailer)
+		{
+			_context = context;
+			_mailer = mailer;
+		}
+
+		[HttpGet]
+		[Route("/nominate")]
+		public IActionResult Nominate()
+		{
+			return View("Nominate");
+		}
+
+	}
+}
